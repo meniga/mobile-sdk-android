7594d8db6b4c380a9882bf928b6f1f89d8f6faa3
Artur Stepniewski <artur@meniga.com>
2018-05-08 11:11:28 +0200

    Add missing suppress lint for MerchantAttribute

--- sdk/src/main/java/com/meniga/sdk/models/merchants/MerchantAttribute.kt
+++ sdk/src/main/java/com/meniga/sdk/models/merchants/MerchantAttribute.kt
@@ -1,23 +1,22 @@
 package com.meniga.sdk.models.merchants
 
-import android.os.Parcel
+import android.annotation.SuppressLint
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
-
 import java.io.Serializable
-import java.util.ArrayList
 
 /**
  * Copyright 2018 Meniga Iceland Inc.
  * Created by agustk on 30.4.2018.
  */
 @Parcelize
+@SuppressLint("ParcelCreator")
 data class MerchantAttribute(
-    val facebook: List<String>? = null,
-    val twitter: List<String>? = null,
-    val instagram: List<String>? = null,
-    val youTube: List<String>? = null,
-    val snapchat: List<String>? = null,
-    val vimeo: List<String>? = null,
-    val mcc: List<Int>? = null
-): Serializable, Parcelable, Cloneable
+        val facebook: List<String>? = null,
+        val twitter: List<String>? = null,
+        val instagram: List<String>? = null,
+        val youTube: List<String>? = null,
+        val snapchat: List<String>? = null,
+        val vimeo: List<String>? = null,
+        val mcc: List<Int>? = null
+) : Serializable, Parcelable, Cloneable
