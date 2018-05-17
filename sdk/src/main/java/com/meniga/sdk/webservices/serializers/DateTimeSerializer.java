b60d7b741e182d5743d250cc25d9ec46534d9f20
Ágúst Karlsson <agustk@meniga.is>
2018-05-17 07:55:43 +0000

    Removing unused imports.

--- sdk/src/main/java/com/meniga/sdk/webservices/serializers/DateTimeSerializer.java
+++ sdk/src/main/java/com/meniga/sdk/webservices/serializers/DateTimeSerializer.java
@@ -9,8 +9,6 @@ import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
 
 import org.joda.time.DateTime;
-import org.joda.time.DateTimeZone;
-import org.joda.time.LocalDateTime;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 import org.joda.time.format.ISODateTimeFormat;
