e615f0a91475ae6025bae48053e3de890dabe5d8
Artur Stepniewski <artur@meniga.com>
2020-05-22 12:19:44 +0200

    Add missing realmId, realmSyncStartDate, end date and status

--- sdk/src/main/java/com/meniga/sdk/models/sync/AccountSyncResult.kt
+++ sdk/src/main/java/com/meniga/sdk/models/sync/AccountSyncResult.kt
@@ -5,16 +5,22 @@ import com.google.gson.annotations.SerializedName
 enum class AccountSyncResult {
     @SerializedName("Success")
     SUCCESS,
+
     @SerializedName("ConsentNotFound")
     CONSENT_NOT_FOUND,
+
     @SerializedName("AccountNotFound")
     ACCOUNT_NOT_FOUND,
+
     @SerializedName("TooManyRequests")
     TOO_MANY_REQUESTS,
+
     @SerializedName("ProviderNotFound")
     PROVIDER_NOT_FOUND,
+
     @SerializedName("ProviderDisabled")
     PROVIDER_DISABLED,
+
     @SerializedName("SyncFailed")
     SYNC_FAILED,
 }
